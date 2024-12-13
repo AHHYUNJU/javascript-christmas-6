c06193bcc6c2cfe3022c0376ae27fd1614e62448
ahhyun <wndkgus0225@naver.com>
2024-12-14 06:00:12 +0900

    5

---
+++ src/message.js
@@ -0,0 +1,11 @@
+export const MESSAGE = {
+  ASK_DATE: "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)",
+  ASK_MENU: "주문하실 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"
+}
+export const ERROR_MESSAGE = {
+  NOT_NUMBER: "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.",
+  NOT_INTEGER: "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.",
+  INVALID_RANGE: "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.",
+  INVALID_ORDER: "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."
+}
+

7f2c2eb5e7b22286f81af5f24eab8dc1432c7d53
ahhyun <wndkgus0225@naver.com>
2024-12-14 05:59:28 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-class App {
-  async run() {}
-}
+import { Console } from '@woowacourse/mission-utils';
+import { askDate, askMenu } from "../src/InputView.js";
+import { MESSAGE } from "../src/message.js";
 
-export default App;
+class App {
+  async run() {
+    const date = await askDate();
+    const menu = await askMenu();
+    Console.print(date);
+    Console.print(menu);
+  }
+}
+const app = new App();
+app.run();
+
+  export default App;

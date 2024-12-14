d3c2dd6a3ca17166031def39c2f2e588b65cdab9
ahhyun <wndkgus0225@naver.com>
2024-12-14 12:50:18 +0900

    4

--- src/message.js
+++ src/message.js
@@ -1,6 +1,9 @@
 export const MESSAGE = {
   ASK_DATE: "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)",
-  ASK_MENU: "주문하실 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"
+  ASK_MENU: "주문하실 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)",
+  SHOW_EVENT: "12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!",
+  ORDER_MENU: "\n<주문 메뉴>",
+  ORIGINAL_TOTAL: "\n<할인 전 총주문 금액>"
 }
 export const ERROR_MESSAGE = {
   NOT_NUMBER: "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.",
